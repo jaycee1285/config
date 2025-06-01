4c0d0cdd58bffef382941f6a08765c2dd5e3d4a9
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-01 11:01:57 -0400

    modified:   modules/networking.nix

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,10 +1,11 @@
 { config, pkgs, ... }:
+
 {
   networking = {
     hostName = "x13";
     networkmanager.enable = true;
-    # wireless.enable = true;
-     # Enable networking
-  programs.nm-applet.enable = true;
+    # wireless.enable = true; # Enable networking
   };
+
+  programs.nm-applet.enable = true;
 }
