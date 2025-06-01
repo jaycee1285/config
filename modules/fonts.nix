82024d9ad2400fc44a7fafaec68834dca8c8bc59
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-01 10:51:38 -0400

    modified:   modules/fonts.nix

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -17,5 +17,7 @@
   pkgs.nerd-fonts.zed-mono
   (google-fonts.override {fonts = [ "Buenard" "Libre Franklin" "Overpass" "Overpass Mono" "Philosopher" "Mulish" "Tenor Sans" "Gentium Book Plus" "Sintony" "Poppins" "Oswald" "Outfit" "Bricolage"
   "Merriweather" "Quattrocento" "Lora" "Raleway" "Cormorant Garamond" "Changa" "Merriweather Sans" "Arsenal" "Inter" "Mulish" "Tauri" "Libre Franklin" "Palanquin" "Tinos" "Source Sans" "Source Serif" ];
+  }
+  )
   ];
 }
