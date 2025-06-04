5ab043d1f59cebbb2f3ad13de0a307318e595e7a
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-04 19:13:08 -0400

    modified:   home/home.nix

--- home/home.nix
+++ home/home.nix
@@ -3,7 +3,7 @@
 {
   home.username = "john";               # Change to your actual user!
   home.homeDirectory = "/home/john";    # Adjust as needed
-
+  home.stateVersion = "25.05";
   programs.home-manager.enable = true;
 
   imports = [
