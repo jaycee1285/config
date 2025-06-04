aae434e30a9dc5bfb91691d15a37872e9c288c24
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-04 13:21:25 -0400

    modified:   super-productivity/flake.nix

--- super-productivity/flake.nix
+++ super-productivity/flake.nix
@@ -1,14 +1,15 @@
 {
-  description = "Always-latest super-productivity fork (no hashes!)";
+  description = "Always-latest super-productivity build from GitHub fork, for Nix flakes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    # Replace this with your actual fork location:
-    super-productivity-src.url = "github:jaycee1285/super-productivity";
     flake-utils.url = "github:numtide/flake-utils";
+    # Replace below with your actual fork and branch/tag as needed:
+    super-productivity-src.url = "github:jaycee1285/super-productivity";
+    # Optionally, to use a branch: super-productivity-src.url = "github:youruser/super-productivity?ref=my-branch";
   };
 
-  outputs = { self, nixpkgs, super-productivity-src, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, super-productivity-src, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
@@ -16,9 +17,9 @@
         superProductivity = pkgs.callPackage ./super-productivity.nix {
           src = super-productivity-src;
         };
+
       in {
         packages.default = superProductivity;
-        # Optional: expose as an app for `nix run`
         apps.default = flake-utils.lib.mkApp { drv = superProductivity; };
       }
     );
