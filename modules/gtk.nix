ef3d8d8e4a96c1b99996eb4516f4b723cf266a92
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-04 18:55:03 -0400

    modified:   configuration.nix 	new file:   modules/gtk.nix

---
+++ modules/gtk.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+  # Set GTK theme and font via dconf/gsettings
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      gtk-theme = "Orchis-Orange-Light-Compact";    # Replace with your theme name
+      icon-theme = "Colloid";        # Optional: icon theme
+      font-name = "Iosevka 12";    # Replace with your desired font and size
+      cursor-theme = "Phinger-Cursors-Light";      # Optional: cursor theme
+    };
+    "org/gnome/desktop/wm/preferences" = {
+      titlebar-font = "Iosevka 12"; # Window title font
+    };
+  };
+}
