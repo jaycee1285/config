32da58bceab2e69d96f5a72e0d9696722f39d656
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-04 13:15:24 -0400

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,21 @@
 {
-  description = "My NixOS system with GTK themes flake";
+  description = "My NixOS system with GTK themes and super-productivity flakes";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     gtk-themes.url = "path:./gtk-themes";
+    super-productivity.url = "path:./super-productivity";  # <--- Added here!
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, gtk-themes, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-unstable, gtk-themes, super-productivity, ... }: {
     nixosConfigurations = {
       john = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
         ];
-        specialArgs = { inherit gtk-themes nixpkgs-unstable; };
+        specialArgs = { inherit gtk-themes nixpkgs-unstable super-productivity; };  # <--- Added here!
       };
     };
   };
