175df0cc9aa1f131828e69af2691c437edc0e5a9
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-04 16:04:32 -0400

    modified:   modules/fonts.nix

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -15,6 +15,7 @@
   pkgs.nerd-fonts.atkynson-mono
   pkgs.nerd-fonts.symbols-only
   pkgs.nerd-fonts.zed-mono
+  pkgs.nerd-fonts.martian-mono
   (google-fonts.override {fonts = [ "Buenard" "Libre Franklin" "Overpass" "Overpass Mono" "Philosopher" "Mulish" "Tenor Sans" "Gentium Book Plus" "Sintony" "Poppins" "Oswald" "Outfit" "Bricolage"
   "Merriweather" "Quattrocento" "Lora" "Raleway" "Cormorant Garamond" "Changa" "Merriweather Sans" "Arsenal" "Inter" "Mulish" "Tauri" "Libre Franklin" "Palanquin" "Tinos" "Source Sans" "Source Serif" ];
   }
