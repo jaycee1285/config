f605080606d01c19e5077461f48966a462b305ec
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-05 13:39:09 -0400

    modified:   home/gtk.nix

--- home/gtk.nix
+++ home/gtk.nix
@@ -5,11 +5,11 @@
     "org/gnome/desktop/interface" = {
       gtk-theme = "Orchis-Orange-Light-Compact";
       icon-theme = "Colloid";
-      font-name = "Iosevka Aile 12";
+      font-name = "Mulish 12";
       cursor-theme = "Phinger-Cursors-Light";
     };
     "org/gnome/desktop/wm/preferences" = {
-      titlebar-font = "Iosevka Aile 12";
+      titlebar-font = "Mulish 12";
     };
   };
 }
