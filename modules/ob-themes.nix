3abe4dc4b3d1368acfae28ae775f83c0b60009f1
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-09 17:31:28 -0400

    new file:   modules/ob-themes.nix 	new file:   ob-themes/flake.nix

---
+++ modules/ob-themes.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ob-themes, ... }:
+
+{
+  environment.systemPackages = [
+    ob-themes.packages.x86_64-linux
+  ]
+}
---
+++ ob-themes/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    ob-themes.url = "github:jaycee1285/ob-themes";
+  };
+
+  outputs = { nixpkgs, ob-themes, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      # NixOS module
+      nixosConfigurations.my-hostname = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          {
+            environment.systemPackages = [
+              ob-themes.packages.${system}.default
+            ];
+          }
+        ];
+      };
+      };
+    };
