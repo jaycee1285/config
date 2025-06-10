790ec80a2e1d3abee66d41a7edce14873e8da4dc
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-09 22:01:34 -0400

    modified:   modules/xserver.nix

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -4,14 +4,14 @@
     enable = true;
     xkb.layout = "us";
     xkb.options = "eurosign:e";
-  #  desktopManager.xfce.enable = true;
-  #  desktopManager.xfce.enableWaylandSession = true;
+    desktopManager.xfce.enable = true;
+    desktopManager.xfce.enableWaylandSession = true;
   #  desktopManager.budgie.enable = true;
   #  desktopManager.lxqt.enable = true;
   };
   services = {
   libinput.enable = true;
-  displayManager.gdm.enable = true;
+  displayManager.ly.enable = true;
   };
   console.keyMap = "us";
 }
