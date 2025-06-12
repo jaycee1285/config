b1c98b8d93baa990eab92ff28f29a724da03b466
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-12 16:03:11 -0400

    modified:   modules/fonts.nix

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -12,6 +12,7 @@
   aleo-fonts
   hubot-sans
   manrope
+  fragment-mono
   pkgs.nerd-fonts.iosevka-term-slab
   pkgs.nerd-fonts.atkynson-mono
   pkgs.nerd-fonts.symbols-only
@@ -24,8 +25,8 @@
   pkgs.nerd-fonts.fantasque-sans-mono
   pkgs.nerd-fonts.agave
   pkgs.nerd-fonts._0xproto
-  (google-fonts.override {fonts = [ "Buenard" "Libre Franklin" "Overpass" "Overpass Mono" "Philosopher" "Mulish" "Tenor Sans" "Gentium Book Plus" "Sintony" "Poppins" "Oswald" "Outfit" "Bricolage"
-  "Merriweather" "Quattrocento" "Lora" "Raleway" "Cormorant Garamond" "Changa" "Merriweather Sans" "Arsenal" "Inter" "Mulish" "Tauri" "Libre Franklin" "Palanquin" "Tinos" "Source Sans" "Source Serif" ];
+  (google-fonts.override {fonts = [ "Buenard" "Libre Franklin" "Overpass" "Overpass Mono" "Epilogue" "Philosopher" "Mulish" "Tenor Sans" "Gentium Book Plus" "Sintony" "Poppins" "Oswald" "Outfit" "Bricolage"
+  "Merriweather" "Quattrocento" "Lora" "Raleway" "Cormorant Garamond" "Changa" "Merriweather Sans" "Arsenal" "Inter" "Spline Sans" "Tauri" "Libre Franklin" "Palanquin" "Tinos" "Source Sans" "Source Serif" ];
   }
   )
   ];
