390da757852de79f852af968ae5114acf3dbbe03
jaycee1285 <132153664+jaycee1285@users.noreply.github.com>
2025-06-13 22:38:52 -0400

    modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -13,29 +13,30 @@
     home-manager.url = "github:nix-community/home-manager";  # <-- Add this!
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, gtk-themes, nur, ob-themes, home-manager, ... }: {
-    nixosConfigurations = {
-      john = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-
-          # Enable Home Manager as a NixOS module
-          home-manager.nixosModules.home-manager
-
-          # Optionally, you can move gtk.nix contents into home.nix, 
-          # or import here as an extra home-manager.users.john = import ./home.nix;
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.backupFileExtension = "backup";
-
-            # Set the user (change "john" to your actual username if different)
-            home-manager.users.john = import ./home/home.nix;
-          }
-        ];
-        specialArgs = { inherit gtk-themes nur ob-themes nixpkgs-unstable; };
+  outputs = { self, nixpkgs, nixpkgs-unstable, gtk-themes, nur, ob-themes, home-manager, ... }:
+let
+  system = "x86_64-linux";
+  pkgs = import nixpkgs {
+    inherit system;
+    overlays = [ nur.overlay ];  # <- Add this line
+  };
+in {
+  nixosConfigurations = {
+    john = nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.users.john = import ./home/home.nix;
+        }
+      ];
+      specialArgs = {
+        inherit gtk-themes ob-themes nixpkgs-unstable nur;
       };
     };
   };
-}
+};
